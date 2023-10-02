c2e486d2ff69a50a7af286906588eeca780f0163
Brent Marquez <brentonmarquez@gmail.com>
2023-10-02 16:11:18 -0600

    use nextauth adapter to save auth and user information in the db automatically on login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,21 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+// import { CredentialsProvider } from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
+import { AuthOptions } from "next-auth";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: 'jwt'
+  }
 };
 
 const handler = NextAuth(authOptions);
