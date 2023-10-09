28e5c346bcd7ed8b5c1c053da0f9b0bfc93f481f
Brent Marquez <brentonmarquez@gmail.com>
2023-10-08 19:29:48 -0600

    fix build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+
 import Image from "next/image";
 // import images as an object
 import myImage from "@/public/images/pic.jpg";
 import { Metadata } from "next";
+import { authOptions } from "./api/auth/authOptions";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
